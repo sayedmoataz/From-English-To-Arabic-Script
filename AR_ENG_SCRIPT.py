771c7077c854a60c9e29f98b5f5f90b04de4f6c1
Sayed Moataz <46425858+sayedmoataz@users.noreply.github.com>
2021-07-30 14:23:14 +0200

    Add files via upload

---
+++ AR_ENG_SCRIPT.py
@@ -0,0 +1,73 @@
+c = input("")
+
+for element in range(0, len(c)):
+    if (ord(c[element])==120 ):
+        print("ء", end="")
+    elif (ord(c[element])==122 ):
+        print("ئ", end="")
+    elif (ord(c[element])==118 ):  
+        print("ر", end="")
+    elif (ord(c[element])==119 ):
+        print("ص", end="")
+    elif (ord(c[element])==115 ):
+        print("س", end="")
+    elif (ord(c[element])==109 ):
+        print("ة", end="")
+    elif (ord(c[element])==117 ):  
+        print("ع", end="")
+    elif (ord(c[element])==104 ):   
+        print("ا", end="")
+    elif (ord(c[element])==113 ):
+        print("ض", end="")
+    elif (ord(c[element])==116 ):
+        print("ف", end="")
+    elif (ord(c[element])==99 ):
+        print("ؤ", end="")
+    elif (ord(c[element])==102 ):
+        print("ب", end="")
+    elif (ord(c[element])==114 ):
+        print("ق", end="")
+    elif (ord(c[element])==96 ):
+        print("ذ", end="")
+    elif (ord(c[element])==97 ):
+        print("ش", end="")
+    elif (ord(c[element])==108 ):
+        print("م", end="")
+    elif (ord(c[element])==110 ):
+        print("ى", end="")
+    elif (ord(c[element])==103 ):
+        print("ل", end="")
+    elif (ord(c[element])==105 ):
+        print("ه", end="")
+    elif (ord(c[element])==46 ):
+        print("ز", end="")
+    elif (ord(c[element])==59 ):
+        print("ك", end="")
+    elif (ord(c[element])==47 ):
+        print("ظ", end="")
+    elif (ord(c[element])==39 ):
+        print("ط", end="")
+    elif (ord(c[element])==44 ):
+        print("و", end="")
+    elif (ord(c[element])==100 ):
+        print("ي", end="")
+    elif (ord(c[element])==121 ):
+        print("غ", end="")
+    elif (ord(c[element])==106 ):
+        print("ت", end="")
+    elif (ord(c[element])==101 ):
+        print("ث", end="")
+    elif (ord(c[element])==91 ):
+        print("ج", end="")
+    elif (ord(c[element])==112 ):
+        print("ح", end="")
+    elif (ord(c[element])==111 ):
+        print("خ", end="")
+    elif (ord(c[element])==93 ):
+        print("د", end="")
+    elif (ord(c[element])==32 ):
+        print(" ", end="")
+
+print("\n")
+
+    
